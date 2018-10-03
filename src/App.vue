2b0ed58e32374073ef649e63fbe2e64e9a17411a
Spencer Alexander <slalexander92@gmail.com>
2018-10-02 17:03:11 -0700

    fix styling, add correct data-flow, combine Controller and AudioPlayer into one component, hide audioplayer, begin working on custom duration bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
+
+  <div id="app"
+    v-if="currentTrack"
+    :style="{ 'background': 'url(' + currentTrack.cover_image + ')' + ' no-repeat' + ' center' + ' center' + ' fixed' }">
 
     <div id="info-bar" class="flex-row">
-      <Controller />
-      <AudioPlayer />
+      <AudioPlayer
+        :track="currentTrack"
+        :artist="artist"
+        v-on:change-song="changeSong"
+        :trackState="trackState" />
     </div>
 
   </div>
+
 </template>
 
 <script>
 import AudioPlayer from './components/AudioPlayer.vue';
-import Controller from './components/Controller.vue';
 
 export default {
   name: 'app',
   components: {
     AudioPlayer,
-    Controller
   },
-  data: () => ({
+  beforeCreate(){
 
-  })
-}
+    fetch("https://s3-us-west-1.amazonaws.com/fbx-fed-homework/fed_home_assignment_api.json")
+      .then( result => result.json() )
+      .then( result => this.album = result )
+      .catch( err => console.log( err ) );
+
+  },
+  watch: {
+    album( album ){
+      Object.assign( this, album );
+    },
+    tracks( tracks ){
+      this.currentTrack = tracks[this.trackId];
+
+      console.log( this.currentTrack );
+    },
+    trackId( id ){
+      this.currentTrack = this.tracks[id];
+    }
+  },
+  data:() => ({
+    album: null,
+    album_name: null,
+    artist : null,
+    tracks: null,
+    trackId : 0,
+    currentTrack : null,
+    trackState:{
+      state:'pause',
+      elapsed:'',
+      duration:'',
+    }
+  }),
+
+  methods:{
+
+    changeSong( direction ){
+
+      switch( direction ){
+
+        case 'next' :
+          if( this.trackId !== this.tracks.length - 1 ){
+            this.trackId += 1;
+          }
+          else{
+            this.trackId = 0;
+          }
+          break;
+        case 'prev' :
+          if( this.trackId !== 0 ){
+            this.trackId -= 1;
+          }
+          else{
+            this.trackId = this.tracks.length - 1;
+          }
+          break;
+
+      }
+    }
+
+  }
+
+
+};
+
 </script>
 
 <style lang="scss" scoped>
--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -2,10 +2,29 @@
 
   <div id="audio-player">
     <div class="flex-row">
-      <p>artist</p>
-      <p>track</p>
+      <div id="controller">
+        <i class="material-icons" @click="prevSong">skip_previous</i>
+        <i class="material-icons" v-show="trackState.state === 'pause' " @click="toggleState( 'play' )">play_arrow</i>
+        <i class="material-icons" v-show="trackState.state === 'play' " @click="toggleState( 'pause' )">pause</i>
+        <i class="material-icons" @click="nextSong">skip_next</i>
+      </div>
+      <div id="player">
+        <div class="flex-row">
+          <p>{{ artist }}</p>
+          <p>{{ track.name }}</p>
+        </div>
+        <div id="control-container" class="flex-column">
+          <div class="progress-bar">
+            <div class="indicator"></div>
+          </div>
+          <div class="flex-row">
+            <p></p>
+            <p>{{ trackState.duration }}</p>
+          </div>
+        </div>
+        <audio id='audio-track' ref="audio" :src='track.url' preload="metadata" ></audio>
+      </div>
     </div>
-    <audio controls autoplay></audio>
   </div>
 
 </template>
@@ -13,9 +32,70 @@
 <script>
 export default {
 
+  props:{
+    artist: String,
+    track: Object,
+    trackState : Object,
+  },
+  watch:{
+    track( track ){
+      this.trackURL = track.url;
+    },
+  },
+  mounted(){
+    const audio = document.getElementById('audio-track');
+
+    audio.addEventListener("loadedmetadata", ( e ) => {
+      const duration = audio.duration;
+      const mins     = parseInt( audio.duration / 60, 10 );
+      const seconds  = parseInt( audio.duration % 60 );
+
+      this.trackState.duration = `${mins}:${seconds}`
+    });
+  },
+  computed(){
+
+  },
+  methods:{
+
+    toggleState( changeState ){
+
+      if( changeState === 'play' ){
+        this.trackState.state = 'play';
+        this.$refs.audio.play();
+      }
+      else{
+        this.trackState.state = 'pause';
+        this.$refs.audio.pause();
+      }
+
+    },
+    nextSong(){
+      this.$emit('change-song', 'next');
+      this.$refs.audio.play();
+    },
+    prevSong(){
+      this.$emit('change-song', 'prev');
+      this.$refs.audio.pause();
+    }
+
+  },
+
 }
 </script>
 
 <style lang="scss" scoped>
+
+@import '../globals/variables';
+
+i.material-icons{
+  cursor: pointer;
+  color: $green;
+  font-size: 61px;
+
+  &:hover{
+    color:$hoverGreen;
+  }
+}
 
 </style>
